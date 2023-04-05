7b0be51f2f4741260f241797f6a9c885253fced1
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-04-05 17:06:00 +0200

    improved categories ui. Added selectin for a certain category

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps{
     selected?:boolean;
@@ -14,12 +17,49 @@ const CategoryBox:React.FC<CategoryBoxProps> = ({
     icon:Icon,
     selected
 }) => {
-  return (
+    const router= useRouter();
+    const params = useSearchParams(); //let me read the query string
+
+    const handleClick= useCallback(()=>{
+
+        let currentQuery = {};
+
+        if(params){
+            currentQuery=qs.parse(params.toString());
+        }
+        //what we want is that when we click on the label Beach, the query gets updated
+        //accordingly, and if we click again we want to reset it
+
+        //in the next snippet we spread the query we get from useSearchParams() and we add the new category
+        const updatedQuery:any = {
+            ...currentQuery,
+            category:label
+        }
+
+        //we remove the query if it's already selected and we click on the icon again ('es /?category=beach')
+        if(params?.get('category')===label){
+            delete updatedQuery.category;
+        }
+
+        //now we generate the url to pass to the router. Pathname is always going to be '/'
+        const url = qs.stringifyUrl({
+            url:'/',
+            query:updatedQuery
+        },{skipNull:true});
+
+        router.push(url);
+        
+    },[label,params,router]);
+
+    
+    return (
     <div
+        onClick={handleClick}
         className=
             {`flex
             flex-col
             items-center
+            justify-center
             gap-2
             p-3
             border-b-2
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,6 +3,7 @@ import {TbBeach} from 'react-icons/tb'
 import {GiWindmill} from 'react-icons/gi'
 import {MdOutlineVilla} from 'react-icons/md'
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
     {
@@ -23,29 +24,39 @@ export const categories = [
 ]
 
 const Categories = () => {
-  return (
-    <Container>
-        <div
-            className='
-            pt-4
-            flex
-            flex-row
-            items-center
-            justify-between
-            overflow-x-auto'>
 
-            {categories.map((item)=>{
-                return(
-                    <CategoryBox
-                    key={item.label}
-                    label={item.label}
-                    description={item.description}
-                    icon={item.icon}
-                />
-                )   
-            })}
-        </div>
-    </Container>
+    const params = useSearchParams();
+    const category = params?.get('category')
+    const pathname=usePathname();
+    const isMainPage:boolean=pathname==='/'
+
+    if (!isMainPage){
+        return null;
+    }
+
+    return (
+        <Container>
+            <div
+                className='
+                pt-4
+                flex
+                flex-row
+                items-center
+                justify-between
+                overflow-x-auto'>
+
+                {categories.map((item)=>{
+                    return(
+                        <CategoryBox
+                        key={item.label}
+                        label={item.label}
+                        selected={category===item.label}
+                        icon={item.icon}
+                    />
+                    )   
+                })}
+            </div>
+        </Container>
   )
 }
 
