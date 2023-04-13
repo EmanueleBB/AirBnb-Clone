0f47e0af4558782fe789358a34a0fff9f3b14753
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-04-13 17:23:27 +0200

    added cloudinary uploading

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import Counter from "../inputs/Counter";
 import CountrySelect from "../inputs/CountrySelect";
+import ImageUpload from "../inputs/ImageUpload";
 
 import { categories } from "../navbar/Categories";
 import Modal from "./Modal"
@@ -58,6 +59,7 @@ const RentModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc=watch('imageSrc');
 
     const Map = useMemo(()=>dynamic(()=>import("../Map"),{ssr:false}),[location])
 
@@ -162,6 +164,23 @@ const RentModal = () => {
                     onChange={(value)=>setCustomValue('bathroomCount',value)}
                 />
                 <hr/>
+            </div>
+        )
+    }
+
+    if(step===STEPS.IMAGES){
+        bodyContent=(
+            <div className='
+                flex flex-col gap-8'
+            >
+                <Heading
+                    title='Add a photo of your place'
+                    subtitle='Show guests what your place looks like!'
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value)=>setCustomValue('imageSrc',value)}
+                />
             </div>
         )
     }
